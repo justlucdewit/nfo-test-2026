5605d7103a2bd20a46c261a7dee3d594b15ed897
justlucdewit <luc.dewit@hotmail.nl>
2026-03-18 14:50:31 +0100

    initial project setup

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onBeforeUnmount, onMounted, shallowRef } from 'vue'
+
+let activeTab = shallowRef('details')
+let certificatesNum = shallowRef(10)
+let agreementsNum = shallowRef(99)
+let interval = shallowRef(null)
+
+onMounted(() => {
+	interval.value = setInterval(() => {
+		certificatesNum.value = Math.floor(Math.random() * 100)
+		agreementsNum.value = Math.floor(Math.random() * 100)
+	}, 2000)
+})
+
+onBeforeUnmount(() => {
+	clearInterval(interval.value)
+})
+</script>
+
+<template>
+	<h1>Let's build some tabs!</h1>
+</template>
